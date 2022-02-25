f57c89474ab6073a6743683fdba1a7aed31f1d16
djokicpn <djokic_kg@hotmail.com>
2022-02-25 17:36:31 +0100

    Revert "Added aliases to composer,"
    This reverts commit 6c52ce31bc25957578bba0416852f15709373e0d.

--- src/EmailAuditServiceProvider.php
+++ src/EmailAuditServiceProvider.php
@@ -15,12 +15,14 @@ class EmailAuditServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        $this->loadRoutesFrom(__DIR__ .     '/routes/web.php');
+        $this->loadViewsFrom(__DIR__ .      '/views', 'LaravelEmailAuditLog');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
         Event::listen(MessageSent::class, EmailHasBeenSentListener::class);
     }
 
     public function register()
     {
-        //
+        // $this->app->register(EmailAuditServiceProvider::class);
     }
 }
